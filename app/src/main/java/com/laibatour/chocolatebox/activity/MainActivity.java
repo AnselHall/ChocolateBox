f1b0486bc6f47bd5b1b38fd95ff78c385103371b
AnselHall <1206313836@qq.com>
2015-09-09 17:45:55 +0800

    3

--- app/src/main/java/com/laibatour/chocolatebox/activity/MainActivity.java
+++ app/src/main/java/com/laibatour/chocolatebox/activity/MainActivity.java
@@ -4,17 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.laibatour.chocolatebox.R;
+import com.laibatour.chocolatebox.constant.ImageUrl;
+import com.laibatour.chocolatebox.utils.http.OkHttpClientManager;
 import com.squareup.okhttp.OkHttpClient;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @InjectView(R.id.main_iv)
+    ImageView main_iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
 
-        OkHttpClient mOkHttpClient = new OkHttpClient();
+        OkHttpClientManager.getDisplayImageDelegate().displayImage(main_iv, ImageUrl.imageUrls[9] + "");
     }
 }
